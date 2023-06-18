e20a4765e257a6d188a5b864e8c3b9f30d5a1881
Jayasatwik123 <132732183+Jayasatwik123@users.noreply.github.com>
2023-06-18 17:44:35 +0530

    Update NewRoom.jsx

--- src/pages/newRoom/NewRoom.jsx
+++ src/pages/newRoom/NewRoom.jsx
@@ -12,7 +12,7 @@ const NewRoom = () => {
   const [hotelId, setHotelId] = useState();
   const [rooms, setRooms] = useState([]);
    
-  const { data, loading, error } = useFetch("https://tourreservation.onrender.com/hotels");
+  const { data, loading, error } = useFetch("https://tourreservation.onrender.com/api/hotels");
 
   const handleChange = (e) => {
     setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
