4087d297815ada28b3abb811fd3e0133ffe806d1
Jayasatwik123 <132732183+Jayasatwik123@users.noreply.github.com>
2023-06-18 20:35:17 +0530

    Update Datatable.jsx

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -10,7 +10,7 @@ const Datatable = ({columns}) => {
   const location = useLocation();
   const path = location.pathname.split("/")[1];
   const [list, setList] = useState({});
-  const { data, loading, error } = useFetch(`/api/${path}`);
+  const { data, loading, error } = useFetch(`/api/hotels/${path}`);
 console.log(list);
   useEffect(() => {
     setList(data);
