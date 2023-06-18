47966f7f6378916e6dbe20f48fea047385ce5f6d
Jayasatwik123 <132732183+Jayasatwik123@users.noreply.github.com>
2023-06-18 22:21:26 +0530

    Update Datatable.jsx

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -11,7 +11,7 @@ const Datatable = ({columns}) => {
   const path = location.pathname.split("/")[1];
 
   const [list, setList] = useState({});
-  const { data, loading, error } = useFetch(`https://tourreservation.onrender.com/api/{path}`);
+  const { data, loading, error } = useFetch(`https://tourreservation.onrender.com/api/${path}`);
   console.log(data);
   useEffect(() => {
     setList(data);
