a4c9ee3b26992378e0630204e564b1a682bb8f81
Jayasatwik123 <132732183+Jayasatwik123@users.noreply.github.com>
2023-06-18 18:56:20 +0530

    Update Datatable.jsx

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -61,7 +61,7 @@ const Datatable = ({columns}) => {
         rowsPerPageOptions={[9]}
         checkboxSelection
         getRowId={(row) => row._id}
-        id={(row) => row._id}
+       
       </DataGrid>
       
     </div>
